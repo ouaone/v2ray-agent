786775c39608a861cad8e8a63458c72366a4e267
mack-a <57424792+mack-a@users.noreply.github.com>
2025-05-08 11:32:34 +0800

    feat(脚本): 恢复sing-box配置

--- install.sh
+++ install.sh
@@ -9809,7 +9809,7 @@ menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
     echoContent green "作者：mack-a"
-    echoContent green "当前版本：v3.4.8"
+    echoContent green "当前版本：v3.4.9"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
